80f7a3061af98c3037b9a51bba2cadd03f958b76
jeff <kloudqp@gmail.com>
2024-02-20 01:18:25 -0500

    Texto correcto

---
+++ my-react-app2/src/components/profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../steelsheet/profile.css';
+
+function Profile(props) {
+    return (
+        <div className='profile-container'>
+            <img className='profile-picture' 
+            src={require(`../pictures/${props.picture}.jpg`)}
+            alt={`../pictures/${props.picture}.background`}/>
+            <div className='profile-info'>
+            <p className='profile-text'>
+                {props.name};
+            </p>
+            <p className='profile-charge'>
+                {props.charge} en {props.job};
+            </p>
+            <p className='profile-review'>
+                {props.review};
+            </p>
+            </div>
+        </div>
+        
+    )
+}
+ 
+export default Profile;
